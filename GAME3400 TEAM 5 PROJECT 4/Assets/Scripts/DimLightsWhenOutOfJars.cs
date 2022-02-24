97fdc25fe2a045ff804ed64fbf5ad67ea395e642
Ryan Gehrlein <ryan.gehrlein@gmail.com>
2022-02-23 23:03:43 -0500

    dim light script

--- GAME3400 TEAM 5 PROJECT 4/Assets/Scripts/DimLightsWhenOutOfJars.cs
+++ GAME3400 TEAM 5 PROJECT 4/Assets/Scripts/DimLightsWhenOutOfJars.cs
@@ -7,15 +7,8 @@ public class DimLightsWhenOutOfJars : MonoBehaviour
     [SerializeField]
     private GameObject lightToDisable;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        this.lightToDisable.SetActive(InventoryManager.instance.GetJarCount() > 0);
     }
 }
